4e64ba45455ab068597c7a7773446ed83b065f5a
alysson912 <almenezes912@gmail.com>
2023-10-17 17:32:19 -0300

    clear

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/IconsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  IconsBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Alysson Menezes on 17/10/23.
+//
+
+import SwiftUI
+
+struct IconsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconsBootcamp()
+}
