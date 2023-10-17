f082d3855800d8beaaecafc7b6448f2206a92499
alysson912 <almenezes912@gmail.com>
2023-10-17 17:49:55 -0300

    done

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/IconsBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/IconsBootcamp.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct IconsBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "cloud.rainbow.half")
+            .resizable()// habilita proportcao
+        // .aspectRatio(contentMode: .fit)
+            .scaledToFit()// define tipo proportcao
+        // .font(.caption) // tamanhos padroes
+        //     .font(.system(size: 450)) // tamanho fixo
+        //.foregroundColor(.yellow) // deprecated
+        //    .foregroundStyle(.red) // cor
+        // outro metodo para definir tamanho
+            .symbolRenderingMode(.multicolor)// padrao de cor apple (do icone)
+            .frame(width: 300, height: 300)
+            .clipped() // corta a imagem na proporcao do quadro
+        
     }
 }
 
