9708677a76a6c459a23613645ee8cbb657d7df1c
alysson912 <almenezes912@gmail.com>
2023-10-28 12:08:16 -0300

    Extracting functions and subviews in SwiftUI | Bootcamp #20

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/UI/ExtractedFunction.swift
@@ -0,0 +1,50 @@
+//
+//  ExtractedFunction.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Alysson Menezes on 27/10/23.
+//
+
+import SwiftUI
+
+struct ExtractedFunction: View {
+    
+    @State var  backGroundColor: Color = Color.yellow
+    
+    var body: some View {
+    
+        ZStack {
+    //background
+            backGroundColor.ignoresSafeArea(.all)
+            
+    // content
+            contentLayer
+        }
+    }
+    
+    var contentLayer: some View {
+        VStack {
+            Text("Title")
+                .font(.largeTitle)
+            
+            Button(action: {
+                buttonPress()
+            }, label: {
+                Text("Press-me")
+                    .font(.headline)
+                    .foregroundStyle(Color.white)
+                    .padding()
+                    .background(Color.cyan)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            })
+        }
+    }
+    
+    func buttonPress(){
+        backGroundColor = Color.pink
+    }
+}
+
+#Preview {
+    ExtractedFunction()
+}
