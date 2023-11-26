1729c81b256b9743655589598b60ccd4c0772c61
alysson912 <almenezes912@gmail.com>
2023-11-26 17:39:49 -0300

    Adding markups and documentation to Swift in Xcode | Bootcamp #45

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/UI/DocumentationBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/UI/DocumentationBootcamp.swift
@@ -8,42 +8,69 @@
 import SwiftUI
 
 struct DocumentationBootcamp: View {
+    //MARK: PROPERTIES
+    
     @State var data: [String] = ["Apple", "Oranges", "Bananas"]
     @State var showAlert: Bool = false
     
-    var body: some View {
-        NavigationStack {
-            ScrollView {
-                Text("Hello")
-                ForEach(data, id: \.self) { name in
-                    Text(name)
-                        .font(.headline)
-                }
-            }
-            .navigationTitle("Documentation")
-            .toolbar{
-                ToolbarItemGroup( placement: .topBarTrailing) {
-                    Button("Alert") {
-                        showAlert.toggle()
-                    }
-                }
-            }
-            .alert("Atenção, error", isPresented: $showAlert) {
-                // action
+    //MARK: BODY
+    var body: some View { // comand + option -> ou <-
+        NavigationStack { // start Nav
+            ZStack {
+                // background
+                Color.cyan.ignoresSafeArea()
                 
-            } message: {
-                getAlert(text: "Texto da mensagem")
-            }
-        }
-        
+                // foreground
+                foregroundLayer
+                    .navigationTitle("Documentation")
+                    .toolbar{
+                        ToolbarItemGroup( placement: .topBarTrailing) {
+                            Button("Alert") {
+                                showAlert.toggle()
+                            }
+                        }
+                    }
+                    .alert("Atenção", isPresented: $showAlert) {
+                        // action
+                        
+                        
+                    } message: {
+                        getAlert(text: "Texto da mensagem")
+                    }
+            }// End ZStack
+        }// End Nav
     }
     
+    /// This is the foreground layer that holds a scrollView.
+    private var foregroundLayer: some View {
+        ScrollView {// start ScrollView
+            Text("Hello")
+            ForEach(data, id: \.self) { name in
+                Text(name)
+                    .font(.headline)
+            }
+        }// END - ScrollView
+    }
+    
+    //MARK: FUNCTIONS
+    
+    /// Get an alert with a specified title
+    ///
+    /// This function creates and returns an alert immediately. The alert will have a title based on the text parameter but it will NOT ha a message.
+    ///
+    ///```
+    ///getAlert( text: "HI") -> Alert(title: Text("HI"))
+    ///```
+    /// - Warning: There is no additional message in this Alert.
+    /// - Parameter text: This is the title for the alert
+    /// - Returns: return an alert with a title
     func getAlert(text: String) -> AnyView {
         return AnyView(Text(text))
     }
-  
+    
 }
-        
-        #Preview {
-            DocumentationBootcamp()
-        }
+//MARK: PREVIEW
+
+#Preview {
+    DocumentationBootcamp()
+}
