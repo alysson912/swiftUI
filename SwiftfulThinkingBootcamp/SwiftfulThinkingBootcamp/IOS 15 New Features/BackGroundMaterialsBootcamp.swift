809fdf34d1fa8f230a4e70c580dedcbbe821a552
alysson912 <almenezes912@gmail.com>
2023-12-11 16:49:06 -0300

    Ainda Brincando com Animações e "System Materials and Backgrounds"

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/IOS 15 New Features/BackGroundMaterialsBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/IOS 15 New Features/BackGroundMaterialsBootcamp.swift
@@ -10,35 +10,64 @@ import SwiftUI
 struct BackGroundMaterialsBootcamp: View {
     @State var titleName = "System Materials and Backgrounds"
     let url = URL(string: "https://picsum.photos/1080/1920")
+    @State var showView: Bool = false
+    @State var titleButton = "UP"
+    @Environment(\.colorScheme) var colorScheme
+    @State private var isEnabled = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            VStack {
-                RoundedRectangle(cornerRadius: 4)
-                    .frame(width: 50, height: 4)
-                    .padding()
-                Spacer()
-                Text(titleName)
-                Spacer()
-            }
-            .frame(height: 350)
-            .frame(maxWidth: .infinity)
-            .background(.ultraThinMaterial)
-            .clipShape(RoundedRectangle(cornerRadius: 30))
-        }
-        .ignoresSafeArea()
-        .background(
-                    AsyncImage(url: url) { returnedImage in
-                        returnedImage
-                            .resizable()
-                            .ignoresSafeArea()
-                          
+        ZStack {
             
-                    } placeholder: {
-                        ProgressView()
+            VStack {
+                Spacer()
+                Button(titleButton) {
+                    showView.toggle()
+                    titleButton = "Down"
+                }
+                .foregroundStyle(colorScheme == .light ? .white : .white)
+                .font(.headline)
+                .fontWeight(.semibold)
+                .padding()
+                .padding(.horizontal, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius: 10)
+                
+                VStack {
+                    
+                    if showView {
+//                        RoundedRectangle(cornerRadius: 4)
+//                            .frame(width: 50, height: 4)
+//                            .padding()
+                        
+                        Text(titleName)
+                        
+                            .frame(height: 350)
+                            .frame(maxWidth: .infinity)
+                            .background(.ultraThinMaterial)
+                            .clipShape(RoundedRectangle(cornerRadius: 30))
+                            .ignoresSafeArea()
+                            .transition(.asymmetric(
+                                insertion: .move(edge: .bottom),
+                                removal: AnyTransition.opacity.animation(.easeInOut)
+                            ))
+                            .animation(.easeInOut)
                     }
+                }
+            }
+        }
+        .background(
+            AsyncImage(url: url) { returnedImage in
+                returnedImage
+                  //  .resizable()
+                    .ignoresSafeArea()
+                
+                
+            } placeholder: {
+                ProgressView()
+            }
         )
+        
     }
 }
 
