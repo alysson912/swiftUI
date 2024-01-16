a1945d66be914b13622e40d4148e991a7f70a97f
alysson912 <almenezes912@gmail.com>
2024-01-16 18:21:45 -0300

    Start Clean project

---
+++ SwiftfulThinkingIntermediate-Level/SwiftfulThinkingIntermediate-Level/SwiftfulThinkingIntermediate_LevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingIntermediate_LevelApp.swift
+//  SwiftfulThinkingIntermediate-Level
+//
+//  Created by Alysson Menezes on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingIntermediate_LevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
