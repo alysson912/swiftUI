2478517a1e9b87ba993ac1d02ff3a47ca7e8b6a6
alysson912 <almenezes912@gmail.com>
2024-02-19 19:51:45 -0300

    adicionando dependencias com swiftPackageManager, configurando firebase com delegate ou da nova forma (com init)

--- swift+FirebaseBootcamp/swift+FirebaseBootcamp/swift_FirebaseBootcampApp.swift
+++ swift+FirebaseBootcamp/swift+FirebaseBootcamp/swift_FirebaseBootcampApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
 
 @main
 struct swift_FirebaseBootcampApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+//    init() {
+//        FirebaseApp.configure()
+//        print("Firebase Configured")
+//    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
