2fb72d7d1f57b00b40a4ff1536b1bfdfd5480fdc
alysson912 <almenezes912@gmail.com>
2024-02-19 19:31:31 -0300

    Start New Project in swiftUI

---
+++ swift+FirebaseBootcamp/swift+FirebaseBootcamp/swift_FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_FirebaseBootcampApp.swift
+//  swift+FirebaseBootcamp
+//
+//  Created by Alysson Menezes on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
